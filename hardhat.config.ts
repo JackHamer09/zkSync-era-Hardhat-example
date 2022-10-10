bffcbd8efe1a7d0e6b20dd9f36ec62998ed2783e
Jack Hamer <jackhamer0990@gmail.com>
2022-10-10 21:39:59 +0300

    fix: update packages

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,12 +4,15 @@ require("@matterlabs/hardhat-zksync-solc");
 
 module.exports = {
   zksolc: {
-    version: "1.1.5",
+    version: "1.2.0",
     compilerSource: "docker",
     settings: {
+      optimizer: {
+        enabled: true,
+      },
       experimental: {
         dockerImage: "matterlabs/zksolc",
-        tag: "v1.1.5"
+        tag: "v1.2.0"
       }
     },
   },
--- utils/displayVerificationInfo.ts
+++ utils/displayVerificationInfo.ts
@@ -6,8 +6,8 @@ import path from "path";
 export default function ({hre, contract, contractConstructorArguments, artifact}: {hre: HardhatRuntimeEnvironment, contract: Contract, contractConstructorArguments: any[], artifact: ZkSyncArtifact}) {
   console.log(`\nVerify the contract: https://explorer.zksync.io/contracts/verify?address=${contract.address}&network=${((hre.config as any).zkSyncDeploy as ZkDeployConfig).ethNetwork}`);
   console.log(`Contract name: ${artifact.contractName}`);
+  console.log(`zkSolc Version: v${(hre.userConfig as any).zksolc.version}`);
   console.log(`Solc Version: ${(hre.userConfig.solidity as SolcUserConfig).version}`);
-  console.log(`Compiler Version: v${(hre.userConfig as any).zksolc.version}`);
   console.log(`Source code file path: ${path.join(__dirname, "../", artifact.sourceName)}`);
   // Get constructor arguments
   const contractInterface = new utils.Interface(artifact.abi);
