6cbb118fb57c382882cd3d1da3c55f63c56c5b07
Jack Hamer <jackhamer0990@gmail.com>
2022-12-27 00:33:03 +0200

    fix: update to latest versions

--- utils/displayVerificationInfo.ts
+++ utils/displayVerificationInfo.ts
@@ -1,10 +1,10 @@
 import { HardhatRuntimeEnvironment, SolcUserConfig } from "hardhat/types";
 import { Contract, utils } from "ethers";
-import { ZkDeployConfig, ZkSyncArtifact } from "@matterlabs/hardhat-zksync-deploy/dist/types";
+import { ZkSyncArtifact } from "@matterlabs/hardhat-zksync-deploy/dist/types";
 import path from "path";
 
 export default function ({hre, contract, contractConstructorArguments, artifact}: {hre: HardhatRuntimeEnvironment, contract: Contract, contractConstructorArguments: any[], artifact: ZkSyncArtifact}) {
-  console.log(`\nVerify the contract: https://explorer.zksync.io/contracts/verify?address=${contract.address}&network=${((hre.config as any).zkSyncDeploy as ZkDeployConfig).ethNetwork}`);
+  console.log(`\nVerify the contract: https://explorer.zksync.io/contracts/verify?address=${contract.address}&network=${((hre.config as any).networks as any).zkSyncTestnet.ethNetwork}`);
   console.log(`Contract name: ${artifact.contractName}`);
   console.log(`zkSolc Version: v${(hre.userConfig as any).zksolc.version}`);
   console.log(`Solc Version: ${(hre.userConfig.solidity as SolcUserConfig).version}`);
--- utils/getWallet.ts
+++ utils/getWallet.ts
@@ -1,11 +1,10 @@
 import { Provider, Wallet } from "zksync-web3";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { getDefaultProvider, utils } from "ethers";
-import { ZkDeployConfig } from "@matterlabs/hardhat-zksync-deploy/dist/types";
 
 export default async function (hre: HardhatRuntimeEnvironment) {
-  const syncProvider = new Provider(((hre.config as any).zkSyncDeploy as ZkDeployConfig).zkSyncNetwork);
-  const ethProvider = getDefaultProvider(((hre.config as any).zkSyncDeploy as ZkDeployConfig).ethNetwork);
+  const syncProvider = new Provider(((hre.config as any).networks as any).zkSyncTestnet.url);
+  const ethProvider = getDefaultProvider(((hre.config as any).networks as any).zkSyncTestnet.ethNetwork);
   
   if (!process.env.WALLET_PRIVATE_KEY) {
     throw new Error("WALLET_PRIVATE_KEY env variable is not set");
