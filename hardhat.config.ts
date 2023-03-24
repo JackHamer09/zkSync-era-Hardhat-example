776a99aadf107f12eb5fc22213fb0d2891da27a1
Jack Hamer <jackhamer0990@gmail.com>
2023-03-24 09:20:14 +0200

    fix: update rpc url

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -13,7 +13,7 @@ module.exports = {
 
   networks: {
     zkTestnet: {
-      url: "https://zksync2-testnet.zksync.dev", // URL of the zkSync network RPC
+      url: "https://testnet.era.zksync.dev", // URL of the zkSync network RPC
       ethNetwork: "goerli", // Can also be the RPC URL of the network (e.g. `https://goerli.infura.io/v3/<API_KEY>`)
       zksync: true,
     },
